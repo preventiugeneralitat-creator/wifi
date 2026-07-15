51e22f2a1f504a7c125950411f345bb60c3521be
preventiugeneralitat-creator <preventiugeneralitat@gmail.com>
2026-07-15 12:49:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'wifi-centros-cache-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './wifi.png'
+];
+
+// Instalar Service Worker y guardar en caché los archivos base
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activar y limpiar cachés antiguas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Responder desde el caché o buscar en red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
